72cce8bda631a0b0acf93a51acd001e31821de5f
JammPhan <96209457+JammPhan@users.noreply.github.com>
2025-10-07 14:33:51 +0100

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <Eigen/Dense>
+using namespace Eigen;
+using namespace std;
+
+
+
+
+vector<double> x;
+
+
+int main() {
+    const int N = 400;
+    const double xrange = 5;
+    const double hbar = 1.054571817e-34;
+    const double m = 1.0;
+    const double k = 1.0;
+    const double dx = 10/N;
+    const double w = 1.0;   
+    cout << hbar*10;
+    return 0;
+}
+
+
+
+
